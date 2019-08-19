1e19ac83964893fffb79c96ed20c00611e0ae498
mcv1993 <mcv1993@gmail.com>
2019-08-19 04:36:54 +0000

    quiz2

--- app/controllers/instigator/cats_controller.rb
+++ app/controllers/instigator/cats_controller.rb
@@ -2,6 +2,22 @@ class Instigator::CatsController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    @cat = Cat.new
+  end
+
+  def create
+    @cat = current_user.cats.create(cat_params)
+    redirect_to instigator_cat_path(@cat)
+  end
+
+  def show
+    @cat = Cat.find(params[:id])
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:title, :description)
   end
 
 end
